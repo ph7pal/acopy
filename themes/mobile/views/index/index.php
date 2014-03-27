c16e2b9ad7ae8665adf35675700c3466e5a6352e
ph7pal <ph7pal@qq.com>
2014-03-27 23:50:16 +0800

    23:50

--- themes/mobile/views/index/index.php
+++ themes/mobile/views/index/index.php
@@ -3,8 +3,10 @@
 <div class="module clear">    
   <div class="wrap clear">
      <?php if(!empty($mainCols)){?> 
-     <?php foreach($mainCols as $mc){?> 
-     <?php if($mc['classify']!='thumb'){?>     
+     <?php foreach($mainCols as $mc){
+         if(!empty($seconds)){
+             $this->renderPartial('/posts/column',array('listposts'=>$seconds,'info'=>$mc));
+         }elseif($mc['classify']!='thumb'){?>     
      <div class="moduleBox">
       <div class="col">
         <div>
--- themes/web/views/posts/column.php
+++ themes/web/views/posts/column.php
@@ -12,14 +12,14 @@
                     ?>
             <li>
                 <p>
-                    <a href="<?php echo Yii::app()->createUrl('posts/show',array('id'=>$_list['id']));?>">
+                    <a href="<?php echo Yii::app()->createUrl('index/index',array('colid'=>$_list['id']));?>">
                         <?php if(!empty($faceImg)){?>
                     <img src="<?php echo zmf::uploadDirs($faceImg['logid'], 'site', $faceImg['classify'], '300').'/'.$faceImg['filePath'];?>"/>
                         <?php }else{ echo zmf::noImg();}?>
                     </a>
                 </p>                    
                 <span>
-                    <?php echo CHtml::link(zmf::subStr($_list['title'],10),array('posts/show','id'=>$_list['id']),array('target'=>'_blank'));?>
+                    <?php echo CHtml::link($_list['title'],array('index/index','colid'=>$_list['id']),array('target'=>'_blank'));?>
                 <div class="post_meta">Posted at <?php echo date('Y/m/d',$_list['cTime']);?></div>                
                 <?php
                     echo CHtml::link('阅读',array('index/index','colid'=>$_list['id']),array('class'=>'arrow_link','target'=>'_blank'));
